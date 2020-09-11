bbf4d54c5824609507e0d6a9fad98c500b0f116b
KIho Mmorita <kiho.h1.chukecheese@gmail.com>
2020-09-11 12:34:07 +0900

    Initial Commit

---
+++ Techmonlfors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Techmonlfors
+//
+//  Created by 森田貴帆 on 2020/09/11.
+//  Copyright © 2020 森田貴帆. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
